7f23c7ae613b56874ae03709e8d812b3d1e28cab
andre23-gif <plessejulien@yahoo.fr>
2026-04-28 20:56:31 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+// ===============================
+// TEST DE CHARGEMENT app.js
+// ===============================
+alert("APP.JS EST BIEN CHARGÉ");
 
+// ===============================
+// TEST DE CLIC (SANS SUPABASE)
+// ===============================
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.querySelector("#auth-form button");
+  if (btn) {
+    btn.addEventListener("click", () => {
+      alert("CLIC CAPTÉ PAR APP.JS");
+    });
+  }
+});
